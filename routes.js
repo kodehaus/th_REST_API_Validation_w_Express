3d6487c62043d477139657fd0384696b4907294c
tpo-krobin13 <krobin13@travelers.com>
2021-03-05 14:00:54 -0500

    Format Validation Error Messages

--- routes.js
+++ routes.js
@@ -20,17 +20,35 @@ router.post('/users', (req, res) => {
   const user = req.body;
   const errors = [];
   if(!user.name){
-    errors.push('Please provide a value for "name"');
+    errors.push(
+      {
+        name: [{
+          message: '"The request body must contain a \"name\" field set to the user\'s name',
+          userMessage: 'Please provide a value for "name"'
+        }]
+      }
+    )
   }
   if(!user.email){
-    errors.push('Please provide a value for "email"');
+    errors.push(
+      {
+        email: [{
+          'message': '"The request body must contain an \"email\" field set to the user\'s email',
+          'userMessage': 'Please provide a value for "email"'
+        }]
+      }
+    )
+  }
+  
+  if(errors.length > 0 ){
+    res.status(400).json({errors});
+  } else {
+    // Add the user to the `users` array.
+    users.push(user);
+    // Set the status to 201 Created and end the response.
+    res.status(201).end();
   }
 
-  // Add the user to the `users` array.
-  users.push(user);
-
-  // Set the status to 201 Created and end the response.
-  res.status(201).end();
 });
 
 module.exports = router;
