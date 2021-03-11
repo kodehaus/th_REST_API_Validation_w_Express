fe4d8a6d98641fd53e54e597538919978c3d774d
tpo-krobin13 <krobin13@travelers.com>
2021-03-11 16:17:00 -0500

    checked error test

--- test/userAccount.test.spec.js
+++ test/userAccount.test.spec.js
@@ -17,6 +17,32 @@ describe('User Insert', function (){
   let expectedStatusCode = 400;
   let actualStatusCode ;
 
+  it('should error without name, email and password', async function () {
+    const res = await axios.post('http://localhost:5000/api/users', userRecord)
+      .then(function (response) {
+        return response
+      })
+      .catch((err) => {
+        let errObj = err.response.data;
+        expectedName = errObj.errors[0].name;
+        expectedEmail = errObj.errors[1].email;
+        expectedPassword = errObj.errors[2].password;
+
+        actualStatusCode = err.response.status;
+      });
+
+    expect(actualStatusCode).to.equal(expectedStatusCode);
+    expect(expectedName[0].message).to.equal("The request body must contain a name field set to the user's name");
+    expect(expectedName[0].userMessage).to.equal("Please provide a value for name");
+
+    expect(expectedEmail[0].message).to.equal("The request body must contain an email field set to the user's email");
+    expect(expectedEmail[0].userMessage).to.equal("Please provide a value for email");
+
+    expect(expectedPassword[0].message).to.equal("The request body must contain a valid password");
+    expect(expectedPassword[0].userMessage).to.equal("Please provide a value for password");
+    
+  });
+
   it('should return a 201 response status code using async/await', async function () {
     userRecord.name = "Ken Robinson";
     userRecord.email = "ken@there.com";
